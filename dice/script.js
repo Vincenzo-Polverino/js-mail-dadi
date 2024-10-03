3acedb283e9280786f01446464635369838af6cb
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-03 15:49:43 +0200

    add:random number from 1 to 6

---
+++ dice/script.js
@@ -0,0 +1,7 @@
+console.log('linked');
+
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+let user_dice = Math.floor(Math.random() * 6) + 1;
+
+let cpu_dice = Math.floor(Math.random() * 6) + 1;

fabc1a7d2b80efc4b939bb169031d20b34570ba6
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-03 15:32:00 +0200

    add: output_message

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let mail_list = ['prova@test.com', 'caio@sempronio.com', 'tizio@qualcosa.com']
 
 // controlla che sia nella lista di chi può accedere
 let present_mail = mail_list.includes(mail)
+
+//stampa un messaggio appropriato sull’esito del controllo.
+if(present_mail === true){
+    console.log('E-mail presente in lista');
+    
+}else{
+    console.log('E-mail non presente in lista');   
+}

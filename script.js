5e1f42018b420872a216b4f25e4123369000ef4f
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-03 15:27:30 +0200

    add prompt

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('linked');
+
+//Chiedi all’utente la sua email
+let mail = prompt('inserisci la tua email')
+
+// controlla che sia nella lista di chi può accedere
+
+//stampa un messaggio appropriato sull’esito del controllo.
